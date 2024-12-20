8e271b8a6b679edf3765ed01c2076c5c90818bd9
nikita.kopot <nikita.kopot@yandex.ru>
2024-12-20 03:45:05 +0300

    feat: add base icon component with few icons and base loader icon

---
+++ src/components/common/BaseSpinner.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseIcon from '@components/common/BaseIcon.vue'
+
+defineProps<{
+  iconWidth: string | number | null
+  iconHeight: string | number | null
+}>()
+</script>
+
+<template>
+  <div class="text-gray-200 animate-spin dark:text-gray-600 fill-primary">
+    <BaseIcon
+      name="loadingSpinnerIcon"
+      v-bind="$props"
+    />
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
